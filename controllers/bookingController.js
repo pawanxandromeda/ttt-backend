314e8fb927591d4ec967073a98ca440a16c8eefc
Pawan <pawanxandromeda@gmail.com>
2025-08-15 12:13:51 +0530

    fix

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -5,7 +5,7 @@ const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
     user: process.env.EMAIL_USER, // your Gmail address
-    pass: process.env.EMAIL_PASS  // your Gmail App Password (NOT regular password)
+    pass: process.env.EMAIL_PASS  
   }
 });
 
