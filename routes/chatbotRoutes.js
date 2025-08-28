0b30faf340dfa36aec2307ea36a03cb6c15838c3
Pawan <pawanxandromeda@gmail.com>
2025-08-29 02:35:07 +0530

    commit

--- routes/chatbotRoutes.js
+++ routes/chatbotRoutes.js
@@ -1,9 +1,7 @@
 const express = require("express");
 const router = express.Router();
-
-// Must match the exact file name: chatbotController.js
 const { handleChat } = require("../controllers/chatbotController");
 
 router.post("/chat", handleChat);
 
-module.exports = router;
+module.exports = router; // ✅ not an object
